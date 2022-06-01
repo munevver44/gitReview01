3e0c3917963689a07cc189c65d0d07e66e917616
munevver44 <munevver1421@gmail.com>
2022-05-31 21:35:51 -0400

    first commit my

---
+++ src/gitReview/gitreview01.java
@@ -0,0 +1,12 @@
+package gitReview;
+
+public class gitreview01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("today we learn github");
+		System.out.println("taday is a very good day");
+		
+	}
+
+}
